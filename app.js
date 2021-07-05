99360b9a0c89a8feb6013a543518aec4a0afcd5b
godzillalogan <kenlogan0117@gmail.com>
2021-07-05 15:22:40 +0800

    2021.7.5 , 外, 做到 S3 U70 美化密碼產生器

---
+++ app.js
@@ -0,0 +1,16 @@
+//透過 require 載入 express 和樣板引擎 express-handlebars
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('handlebars',exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/',(req, res) => {
+    res.render('index')
+})
+
+app.listen(port, ()=>{
+    console.log(`Express app listening on port ${port}.`)
+})
